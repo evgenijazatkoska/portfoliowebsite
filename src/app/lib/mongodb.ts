c21d8a15ffe1fbf2f2000cf64ecb9d134bfe3f0b
Evgenija Zatkoska <ezatkoska@yahoo.com>
2025-03-17 15:03:26 +0100

    route.ts update

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,19 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         if (mongoose.connection.readyState===0) {
-//             await mongoose.connect(process.env.MONGODB_URI as string);
-//             console.log ('db connected')
-//         }
-//     } catch(error){
-//         console.log(error)
-//     }
-// }
-
-// export default connectDB;
-
-// lib/mongodb.ts
 import mongoose from "mongoose";
 
 const connectDB = async () => {
